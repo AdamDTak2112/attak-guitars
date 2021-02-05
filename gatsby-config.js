d50e43843c46b0594a435dae7f1313d28fab29f2
Adam Tak <adam.tak2112@gmail.com>
2021-02-05 12:24:45 -0700

    install gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,6 @@ module.exports = {
     title: "Attak Guitars",
   },
   plugins: [
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
   ],
